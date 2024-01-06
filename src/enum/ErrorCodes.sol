a46de2ec9f46fa1036fb1d9bc8ecf8f1a9bf4f05
calvinchang <q20274982@gmail.com>
2024-01-06 14:45:59 +0800

    feat: add ErrorCodes enum

---
+++ src/enum/ErrorCodes.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+enum ErrorCodes {
+    OK, // OK
+    HAS_MARKED, // 已經標記過
+    INVALID, // 無效的
+    UN_MARKED, // 未標記過
+    INVALID_AMOUNT, // 無效的金額
+    NOT_MARKER,
+    NOT_ABLE_TO_ClAIM,
+    UNAUTHORIZED,
+    UNEXPECTED
+}
